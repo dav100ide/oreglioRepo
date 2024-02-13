571bb221ad6d26b5aec97bbbeb24ec71deb6a1f4
dav100ide <oreglio.dev@gmail.com>
2024-02-13 20:07:57 +0100

    add 2 empty libs

---
+++ libs/base/src/lib/base/base.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'lib-base',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './base.component.html',
+    styleUrl: './base.component.css',
+})
+export class BaseComponent {}
---
+++ libs/utility/src/lib/utility/utility.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'lib-utility',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './utility.component.html',
+    styleUrl: './utility.component.css',
+})
+export class UtilityComponent {}
